f58a4171159ab75707f84130029a0e2ab2c84dc1
frankadrian <frankwebdevelopment@gmail.com>
2014-06-08 18:49:37 +0200

    Update the way words are counted

--- app/scripts/controllers/typetest.js
+++ app/scripts/controllers/typetest.js
@@ -4,14 +4,22 @@ angular.module('typesterApp')
     .controller('TypetestCtrl', function ($scope) {
         //$scope.timer;
         $scope.avgWpm = 0;
+        $scope.wordCount = 0;
         $scope.userInput = '';
         $scope.previewText = 'One morning, when Gregor Samsa woke from troubled dreams, he found himself transformed in his bed into a horrible vermin. He lay on his armour-like back, and if he lifted his head a little he could see his brown belly, slightly domed and divided by arches into stiff sections. The bedding was hardly able to cover it and seemed ready to slide off any moment. His many legs, pitifully thin compared with the size of the rest of him, waved about helplessly as he looked. "What\'s happened to me?" he thought. It wasn\'t a dream. His room, a proper human';
         $scope.result = {};
 
+        function resetScope () {
+            $scope.avgWpm = 0;
+            $scope.wordCount = 0;
+            $scope.userInput = '';
+            $scope.result = {};
+        }
+
         $scope.startTimer = function () {
             $scope.$broadcast('timer-start');
             $scope.timerRunning = true;
-            $scope.userInput = '';
+            resetScope();
 
             //for tests to pass
             var inputElement = document.getElementById('input-text');
@@ -34,13 +42,13 @@ angular.module('typesterApp')
         $scope.userInputChanged = function (event) {
 
             if ($scope.timerRunning) {
-                $scope.wordCount = $scope.userInput.split(' ').length;
-                //update avg wmp
-
                 //if ctrl+enter was pressed stop timer.
                 if (event.ctrlKey && event.keyCode === 13) {
                     $scope.stopTimer();
                 }
+                //update wordcount
+                //maybe consider using 5 char = 1 word
+                $scope.wordCount = $scope.userInput.length / 5;
             }
         };
 
@@ -66,8 +74,8 @@ angular.module('typesterApp')
                 i = 0, len = boilerTextArr.length;
 
             for (; len > i; i++) {
-                var neededWord = boilerTextArr[i];
-                var typedWord = typedTextArr[i];
+                var neededWord = boilerTextArr[i],
+                    typedWord = typedTextArr[i];
 
                 if (typedWord) {
 
